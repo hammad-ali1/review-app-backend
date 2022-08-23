2baee68f3e68f10ccdd1b4b0608350cb1faa71ba
Hammad Ali <hammad.a22002@gmail.com>
2022-08-23 17:03:31 +0500

    Update employee.controllers.ts

--- src/controllers/employee.controllers.ts
+++ src/controllers/employee.controllers.ts
@@ -1,18 +1,21 @@
 import asyncHandler from "express-async-handler";
 import Employee from "../models/Employee.model";
 
-export const getEmployees = asyncHandler(async (req, res) => {
+export const getEmployees = asyncHandler(async (req, res): Promise<any> => {
   try {
     const { name } = req.query;
+    if (!name)
+      return res
+        .status(400)
+        .json({ message: "Name is a required query parameter" });
     const employees = await Employee.find({
       name: new RegExp(name as string),
     });
-    res.status(200).json({ success: true, employees });
-  } catch (err) {
-    console.log(err);
+    res.status(200).json({ results: employees });
+  } catch (err: any) {
+    console.log(err.message);
     res.status(400).json({
       success: false,
-      err: err,
     });
   }
 });
