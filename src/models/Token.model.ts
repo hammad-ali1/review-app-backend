a6f24c1b3f5d8b62c73179a93868c7e675b06d18
Hammad Ali <hammad.a22002@gmail.com>
2022-08-25 21:10:11 +0500

    add timestamps

--- src/models/Token.model.ts
+++ src/models/Token.model.ts
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const TokenSchema = new Schema({
-  token: { type: String, required: true, unique: true },
-  user: { type: String, required: true, unique: true, ref: "User" },
-});
+const TokenSchema = new Schema(
+  {
+    token: { type: String, required: true, unique: true },
+    user: { type: String, required: true, unique: true, ref: "User" },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Token = mongoose.model("Token", TokenSchema);
 
--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -9,26 +9,31 @@ interface UserModelInterface extends Model<UserInterface> {
   //static method declarations
 }
 
-const UserScehma = new Schema<GlobalTypes.User>({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    lowercase: true,
-    validate: {
-      validator: (email: string) =>
-        new RegExp(
-          "^[a-z0-9]{4}-[a-z]{3}-[0-9]{3}(@cuilahore.edu.pk)$",
-          "i"
-        ).test(email),
-      message: "email does not match the cuilahore domain",
+const UserScehma = new Schema<GlobalTypes.User>(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      validate: {
+        validator: (email: string) =>
+          new RegExp(
+            "^[a-z0-9]{4}-[a-z]{3}-[0-9]{3}(@cuilahore.edu.pk)$",
+            "i"
+          ).test(email),
+        message: "email does not match the cuilahore domain",
+      },
+      immutable: true,
     },
-    immutable: true,
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    isEmailVerified: { type: Boolean, default: false },
   },
-  name: { type: String, required: true },
-  password: { type: String, required: true },
-  isEmailVerified: { type: Boolean, default: false },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model<UserInterface, UserModelInterface>(
   "User",
