00403c078ca6418a84748537a70f8e33b4db7bcd
Hammad Ali <hammad.a22002@gmail.com>
2022-10-14 22:17:17 +0500

    dashboard

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -1,5 +1,7 @@
 import asyncHandler from "express-async-handler";
+import mongoose from "mongoose";
 import Transaction from "../models/Transaction.model";
+import Asset from "../models/Asset.model";
 
 export const addTransaction = asyncHandler(async (req, res): Promise<any> => {
   const transaction: GlobalTypes.Transaction = req.body;
@@ -55,6 +57,76 @@ export const deleteTransaction = asyncHandler(
       res
         .status(400)
         .json({ message: "error occured while deleting transaction" });
+    }
+  }
+);
+
+export const getTransactionsSummary = asyncHandler(
+  async (req, res): Promise<any> => {
+    const { user } = req.params;
+    try {
+      const cashFlowSummary = await Transaction.aggregate([
+        {
+          $match: {
+            user: new mongoose.Types.ObjectId(user),
+          },
+        },
+        { $group: { _id: "$inflowOrOutflow", total: { $sum: "$amount" } } },
+      ]);
+      const assetSummary = await Asset.aggregate([
+        {
+          $match: {
+            user: new mongoose.Types.ObjectId(user),
+          },
+        },
+        {
+          $group: {
+            _id: "$user",
+            total: { $sum: "$price" },
+          },
+        },
+      ]);
+      const cashFlowDetail = await Transaction.aggregate([
+        {
+          $match: {
+            user: new mongoose.Types.ObjectId("6337fcde1e2798cc6b20688b"),
+          },
+        },
+
+        {
+          $sort: {
+            dateOfTransaction: 1,
+          },
+        },
+        {
+          $group: {
+            _id: "$dateOfTransaction",
+            result: { $push: { type: "$inflowOrOutflow", amount: "$amount" } },
+          },
+        },
+      ]);
+
+      const assetDetail = await Asset.aggregate([
+        { $match: { user: new mongoose.Types.ObjectId(user) } },
+
+        {
+          $sort: {
+            acquiredDate: 1,
+          },
+        },
+      ]);
+      const result = {
+        cashFlowDetail,
+        cashFlowSummary,
+        assetDetail,
+        assetSummary,
+      };
+      return res.status(200).json(result);
+    } catch (err: any) {
+      console.log(err.message);
+      res
+        .status(400)
+        .json({ message: "error occured while fetching transaction" });
     }
   }
 );
--- src/router/transaction.routes.ts
+++ src/router/transaction.routes.ts
@@ -4,11 +4,13 @@ import {
   addTransaction,
   getTransactions,
   deleteTransaction,
+  getTransactionsSummary,
 } from "../controllers/transaction.controller";
 const transactionRouter = Router();
 
 transactionRouter.route("/add").post(addTransaction);
 transactionRouter.route("/:user").get(getTransactions);
 transactionRouter.route("/delete/:_id").post(deleteTransaction);
+transactionRouter.route("/sum/:user").get(getTransactionsSummary);
 
 export default transactionRouter;
